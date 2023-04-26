0bcd0f4ff9f8856964e2119cabb00f1b1c7f80c0
Dhanraj <dhanrajmankar30@gmail.com>
2023-04-26 20:40:40 +0530

    This Is UC4

---
+++ Day7Java/src/UC4/Gambler.java
@@ -0,0 +1,17 @@
+package UC4;
+
+
+public class Gambler {
+	
+	private int stake=100;
+	
+	public int getStake() {
+		return stake;
+	}
+	public void setStake(int stake) {
+		this.stake = stake;
+	}
+	
+
+}
+
